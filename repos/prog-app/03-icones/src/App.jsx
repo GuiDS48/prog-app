6dc92850d217fedc653e5fda89e98b15d1891b04
GUILHERME DANIEL SILVA <guilherme_silva183@estudante.sesisenai.org.br>
2026-03-11 21:34:01 -0300

    aula 03 prog app

---
+++ repos/prog-app/03-icones/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import './componestes/Calibragem'
+import Calibragem from './componestes/Calibragem';
+
+function App() {
+  const [inputNome, setInputNome] = useState('')
+
+  function cadastrar(){
+    console.log("cadastrando "+ inputNome)
+  }
+
+  return (
+    <>
+      <h1>oieeee</h1>
+      Nome <input type="text"
+        value={inputNome}
+        onChange={(e) => setInputNome(e.target.value)}
+
+      />
+      <button onClick={cadastrar}>Cadastro</button>
+      <Calibragem />
+
+    </>
+  )
+}
+
+export default App
---
+++ repos/prog-app/03-icones/src/componestes/Calibragem.jsx
@@ -0,0 +1,18 @@
+
+
+
+function Calibragem() {
+
+
+
+    return (
+        <div>
+            <h2>calculo</h2>
+            <input type="text"
+            />
+            
+        </div>
+    )
+}
+
+export default Calibragem
